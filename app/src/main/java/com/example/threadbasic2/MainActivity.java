0b9212786ad1ebcd80cfeee91f2e542dad7d0381
ShimWooJeong <dnwjd040635@gmail.com>
2021-06-03 18:03:07 +0900

    Multi-threading 1

--- app/src/main/java/com/example/threadbasic2/MainActivity.java
+++ app/src/main/java/com/example/threadbasic2/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     Thread wr;
     boolean running = true;
     final String TAG = "THREAD2";
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,44 +20,60 @@ public class MainActivity extends AppCompatActivity {
     @Override
 protected void onStart() {
         super.onStart();
-
+        i = 0;
         running = true;
         wr = new Thread(new Runnable() {
             @Override
             public void run() {
-                int i = 0;
-                for(i = 0; i < 20 && running; i++){
+                for(i = 0; i < 20 && running;){
                     try{
                         Thread.sleep(1000);
                     }catch (InterruptedException e) {
                     }
                 }
-                Log.v(TAG, "Runnable time=" + i);
+                Log.v(TAG, "Thread time=" + i);
             }
         });
         wr.start();
         Log.v(TAG, "Now I am in onStart");
+        Log.v(TAG, "Thread time=" + i);
     }
 
     @Override
     public void onStop() {
         super.onStop();
         running = false;
+        i++;
+
         Log.v(TAG, "Now I am in onStop");
+        Log.v(TAG, "Runnable time=" + i);
+        Log.v(TAG, "Thread time=" + i);
+
     }
 
     @Override
     public void onPause() {
         super.onPause();
         running = false;
+        i++;
+
         Log.v(TAG, "Now I am in onPause");
+        Log.v(TAG, "Runnable time=" + i);
+        Log.v(TAG, "Thread time=" + i);
+        i++;
+        Log.v(TAG, "Runnable time=" + i);
+        Log.v(TAG, "Thread time=" + i);
     }
 
     @Override
     public void onResume() {
         super.onResume();
         running = false;
+        i++;
+
         Log.v(TAG, "Now I am in onResume");
+        Log.v(TAG, "Runnable time=" + i);
+        Log.v(TAG, "Thread time=" + i);
     }
 
 }
